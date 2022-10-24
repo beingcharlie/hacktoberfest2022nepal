2323ee7243f9002e34c03648e0ebaf11e66317bc
SURESH KUMAR MAHATO <94044416+beingcharlie@users.noreply.github.com>
2022-10-24 08:59:57 +0545

    Create areaoftraingle.c
    We can find out the area of a triangle using this program.

---
+++ areaoftraingle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	 int a[10] = {45,46,34,23,48,4,5,6,7};
+	 int i;
+	 printf("Array element are:\n");
+	for(i=0 ; i<10 ; i++)
+	{
+		printf("a[%d]=%d\t",i,a[i]);
+		
+	}
+	return 0;
+	  
+}
